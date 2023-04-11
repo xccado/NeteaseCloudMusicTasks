842f2010f25eb6b1962d8a5d6cfc3f9f160e78ba
xccado <100438949+xccado@users.noreply.github.com>
2023-04-11 13:36:25 +0800

    Update ql_update.py

--- ql_update.py
+++ ql_update.py
@@ -15,21 +15,21 @@ def writeSha(path, file):
 
 
 if __name__ == "__main__":
-    github_url = 'https://github.com/chen310/NeteaseCloudMusicTasks.git'
+    github_url = 'https://github.com/xccado/NeteaseCloudMusicTasks.git'
 
-    if os.path.exists('/ql/data/scripts/chen310_NeteaseCloudMusicTasks'):
+    if os.path.exists('/ql/data/scripts/xccado_NeteaseCloudMusicTasks'):
         data_path = '/ql/data/'
-    elif os.path.exists('/ql/scripts/chen310_NeteaseCloudMusicTasks'):
+    elif os.path.exists('/ql/scripts/xccado_NeteaseCloudMusicTasks'):
         data_path = '/ql/'
     else:
         print('未找到路径，请尝试升级青龙')
         exit(1)
     
-    scripts_config =  data_path + 'scripts/chen310_NeteaseCloudMusicTasks/config.json'
-    old_config = data_path + 'scripts/chen310_NeteaseCloudMusicTasks/config.old.json'
-    example_config = data_path + 'scripts/chen310_NeteaseCloudMusicTasks/config.example.json'
-    repo_config = data_path + 'repo/chen310_NeteaseCloudMusicTasks/config.example.json'
-    dependencies = data_path + 'repo/chen310_NeteaseCloudMusicTasks/requirements.txt'
+    scripts_config =  data_path + 'scripts/xccado_NeteaseCloudMusicTasks/config.json'
+    old_config = data_path + 'scripts/xccado_NeteaseCloudMusicTasks/config.old.json'
+    example_config = data_path + 'scripts/xccado_NeteaseCloudMusicTasks/config.example.json'
+    repo_config = data_path + 'repo/xccado_NeteaseCloudMusicTasks/config.example.json'
+    dependencies = data_path + 'repo/xccado_NeteaseCloudMusicTasks/requirements.txt'
     if os.path.exists(scripts_config):
         print('备份配置文件...')
         os.system('cp -f {} {}'.format(scripts_config, old_config))
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         os.system('cp -f {} {}'.format(repo_config, example_config))
         writeSha(os.path.dirname(repo_config), example_config)
         print('更新配置文件...')
-        os.system('python3 {}scripts/chen310_NeteaseCloudMusicTasks/updateconfig.py {} {} {}'.format(
+        os.system('python3 {}scripts/xccado_NeteaseCloudMusicTasks/updateconfig.py {} {} {}'.format(
             data_path, example_config, scripts_config, scripts_config))
         print('更新完成')
     else:
